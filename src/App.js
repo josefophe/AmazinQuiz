992f891fd05e44d6aa75e75c3ec01ba1f9940850
Joseph Ebin <josefophe@gmail.com>
2024-02-15 13:32:20 +0100

    Quick fix

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ export default function App() {
             <div className="mx-auto max-w-2xl pt-16">
               <div className="text-center">
                 <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                  Sample Juno Quiz App
+                  Amazin Juno Quiz App
                 </h1>
                 <p className="mt-6 text-lg leading-8 text-gray-600">
                   A sample app build with React, Tailwind and{" "}
